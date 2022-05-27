f4e091a181ae3469899197ae7314b710dc65a586
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2022-05-27 07:50:40 -0700

    Create lib.rs

---
+++ lib.rs
@@ -0,0 +1,15 @@
+// transition to different states
+enum ticketPreSale{
+  started,
+  ended
+}
+
+
+// collection
+pub struct favSongs {
+  songs: LookupMap<accountId, favsongs>
+    }
+    
+//storage
+    
+//context, env
